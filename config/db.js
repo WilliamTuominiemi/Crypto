e7632b3622dac770a3390ec9e50df770dfd8410f
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2021-03-23 21:48:44 +0200

    added local db URI

--- config/db.js
+++ config/db.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose')
 
 const connectDB = () => {
 	mongoose
-		.connect(process.env.MONGO_URI, {
+		.connect(process.env.MONGO_URI, { // 'mongodb://127.0.0.1:27017'
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			useFindAndModify: false,
 		})
 		.then(() => {
+
+
 			console.log('connected to db')
 		})
 		.catch((err) => {
