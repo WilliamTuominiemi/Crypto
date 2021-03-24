0f6a4101033ce37980c47bd7572b3f28c90b7515
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2021-03-24 22:04:39 +0200

    fixed route

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ router.get('/', indexController.main)
 router.get('/send', indexController.send_page)
 router.get('/mine', indexController.mine_page)
 router.get('/blockchain', indexController.blockchain)
-router.get('/coinflip', indexController.coinflip)
 
+router.post('/coinflip', indexController.coinflip)
 router.post('/mine', indexController.mine)
 router.post('/send', indexController.send)
 
